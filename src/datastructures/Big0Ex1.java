00835bb82e2c003d868809d97bff0473a4653dc9
somewhereinlondon <Robel.rh123@gmail.com>
2026-05-16 14:56:02 +0100

    learn: Big O - O(1) O(n) O(n²) O(a+b) implementations

---
+++ src/datastructures/Big0Ex1.java
@@ -0,0 +1,8 @@
+package datastructures;
+
+public class Big0Ex1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/datastructures/BigOEx2.java
@@ -0,0 +1,8 @@
+package datastructures;
+
+public class BigOEx2 {
+
+    public static void main(String[] args) {
+        
+    }
+}
