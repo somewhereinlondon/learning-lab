a1345ef0304853affd057e9d0e3bdb704464aa28
somewhereinlondon <Robel.rh123@gmail.com>
2026-08-30 15:33:15 +0100

    week-01: debugger drill, breakpoints and stepping

---
+++ week-01/DebugDrill.java
@@ -0,0 +1,10 @@
+public class DebugDrill {
+   public static void main(String[]args){
+       int total = 0;
+       for (int i = 0; i == 7; i++){
+           total = total + i;
+           System.out.println("i = " + i + " total = " + total);
+       }
+       System.out.println("Final: " + total);
+   }
+}
---
+++ week-01/NullDemo.java
@@ -0,0 +1,8 @@
+public class NullDemo {
+    public static void main(String[]args){
+        String name = null;
+        System.out.println(name.length());
+
+    }
+
+}
